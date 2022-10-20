7a6a99e49341bad7a217da3d12b8c7b95a1e4baf
Ltk2106 <115920256+Ltk2106@users.noreply.github.com>
2022-10-20 09:10:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Ngọc Anh';
+const giftUrl = 'https://www.facebook.com/NganhDo23';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Dear Ngọc Anh';
+const contentCard = 'Nhân ngày 20/10, tớ chúc Ngọc Anh luôn vui vẻ, yêu đời, ngày càng xinh đẹp, học giỏi.Và trên hết tớ mong rằng NA sẽ luôn gặp thuận lợi trong cuộc sống,vơi đi nỗi buồn và càng nhiều niềm vui.';
+
+
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'NA.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
